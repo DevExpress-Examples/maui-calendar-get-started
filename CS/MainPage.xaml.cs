406ead9e704cea5261518c012d5adce2c4688dc4
gorbounoff <28042574+gorbounoff@users.noreply.github.com>
2022-05-11 10:08:07 +0200

    DevExpress Calendar for .NET MAUI

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DevExpress.Maui.Core.Themes;
+using DevExpress.Maui.Editors;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace CalendarExample {
+    public partial class MainPage : ContentPage {
+        public static readonly BindablePropertyKey OrientationPropertyKey = BindableProperty.CreateReadOnly("Orientation", typeof(StackOrientation), typeof(MainPage), StackOrientation.Vertical);
+        public static readonly BindableProperty OrientationProperty = OrientationPropertyKey.BindableProperty;
+        public StackOrientation Orientation => (StackOrientation)GetValue(OrientationProperty);
+
+        public MainPage() {
+            InitializeComponent();
+            ViewModel = new ViewModel();
+            BindingContext = ViewModel;
+        }
+
+        ViewModel ViewModel { get; }
+
+        void CustomDayCellStyle(object sender, CustomSelectableCellStyleEventArgs e) {
+            if (e.Date == DateTime.Today)
+                return;
+
+            if (ViewModel.SelectedDate != null && e.Date == ViewModel.SelectedDate.Value)
+                return;
+
+            SpecialDate specialDate = ViewModel.TryFindSpecialDate(e.Date);
+            if (specialDate == null)
+                return;
+
+            e.ViewInfo.FontAttributes = FontAttributes.Bold;
+            Color textColor;
+            if (specialDate.IsHoliday) {
+                textColor = (Color)Resources["CalendarViewHolidayTextColor"];
+                e.ViewInfo.EllipseBackgroundColor = Color.FromRgba(textColor.Red, textColor.Green, textColor.Blue, 0.25);
+                e.ViewInfo.TextColor = textColor;
+
+                return;
+            }
+            textColor = (Color)Resources["CalendarViewTextColor"];
+            e.ViewInfo.EllipseBackgroundColor = Color.FromRgba(textColor.Red, textColor.Green, textColor.Blue, 0.15);
+            e.ViewInfo.TextColor = textColor;
+        }
+
+        protected override void OnSizeAllocated(double width, double height) {
+            base.OnSizeAllocated(width, height);
+            SetValue(OrientationPropertyKey, width > height ? StackOrientation.Horizontal : StackOrientation.Vertical);
+        }
+    }
+}
+
---
+++ CS/ViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using DevExpress.Maui.Editors;
+
+namespace CalendarExample {
+    public class ViewModel : INotifyPropertyChanged {
+        DateTime displayDate;
+        DateTime? selectedDate;
+        DXCalendarViewType activeViewType;
+        bool isHolidaysAndObservancesListVisible;
+        IEnumerable<SpecialDate> specialDates;
+
+        public ViewModel() {
+            DisplayDate = DateTime.Today;
+            UpdateHolidaysAndObservancesListVisible();
+        }
+
+        public IEnumerable<SpecialDate> SpecialDates {
+            get => this.specialDates;
+            set {
+                if (this.specialDates == value)
+                    return;
+
+                this.specialDates = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DateTime DisplayDate {
+            get => this.displayDate;
+            set {
+                if (this.displayDate == value)
+                    return;
+
+                this.displayDate = value;
+                UpdateCurrentCalendarIfNeeded();
+                SpecialDates = USCalendar.GetSpecialDatesForMonth(DisplayDate.Month);
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DateTime? SelectedDate {
+            get => this.selectedDate;
+            set {
+                if (this.selectedDate == value)
+                    return;
+
+                this.selectedDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DXCalendarViewType ActiveViewType {
+            get => this.activeViewType;
+            set {
+                if (this.activeViewType == value)
+                    return;
+
+                this.activeViewType = value;
+                UpdateHolidaysAndObservancesListVisible();
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool IsHolidaysAndObservancesListVisible {
+            get => this.isHolidaysAndObservancesListVisible;
+            set {
+                if (this.isHolidaysAndObservancesListVisible == value)
+                    return;
+
+                this.isHolidaysAndObservancesListVisible = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        USCalendar USCalendar { get; set; }
+
+        public SpecialDate TryFindSpecialDate(DateTime date) {
+            return SpecialDates.FirstOrDefault(x => x.Date == date);
+        }
+
+        void UpdateHolidaysAndObservancesListVisible() {
+            IsHolidaysAndObservancesListVisible = ActiveViewType == DXCalendarViewType.Day;
+        }
+
+        void UpdateCurrentCalendarIfNeeded() {
+            if (USCalendar == null || USCalendar.Year != DisplayDate.Year)
+                USCalendar = new USCalendar(DisplayDate.Year);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
